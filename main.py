3e66adbc70b54ebb2fd8c3ab12aa2466483176d1
mamg22 <45301823+mamg22@users.noreply.github.com>
2025-04-13 17:58:54 -0400

    server: improve url handling

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("templates"))
 aiohttp_jinja2.get_env(app).globals["tmx"] = tmx
 
 
-API_URL = "https://tmnf.exchange/api"
-TRACK_API_ENDPOINT = API_URL + "/tracks"
+BASE_URL = yarl.URL("https://tmnf.exchange")
+API_URL = BASE_URL / "api"
 
 TRACK_LIST_FIELDS = "TrackId,TrackName,Authors[],Tags[],AuthorTime,Difficulty"
 
 
 async def fetch_track_list(params: Mapping[str, str]):
-    url = TRACK_API_ENDPOINT + "?" + uparse.urlencode(params)
+    url = (API_URL / "tracks").with_query(params)
 
     session = app["client_session"]
     async with session.get(url) as res:
@@ -61,7 +61,9 @@ async def track_image(request: Request):
     trackid = request.match_info["trackid"]
     session = app["client_session"]
 
-    async with session.get(f"https://tmnf.exchange/trackshow/{trackid}/image/1") as res:
+    async with session.get(
+        BASE_URL.joinpath("trackshow", trackid, "image", "1")
+    ) as res:
         response = web.Response(body=(await res.read()))
         response.content_type = "image/jpeg"
         return response
@@ -78,7 +80,7 @@ async def track_details(request: Request):
             "fields": "TrackId,TrackName,AuthorTime,GoldTarget,SilverTarget,BronzeTarget,Uploader.Name,Difficulty,Routes,Mood,Tags,"
             "Awards,Comments,ReplayType,TrackValue,PrimaryType,Car,Environment",
         }
-        track_url = yarl.URL(TRACK_API_ENDPOINT).with_query(track_query)
+        track_url = (API_URL / "tracks").with_query(track_query)
         track_task = tg.create_task(session.get(track_url))
 
         replay_query = {
@@ -86,7 +88,7 @@ async def track_details(request: Request):
             "best": 1,
             "fields": "User.Name,ReplayTime,Position",
         }
-        replay_url = yarl.URL(API_URL + "/replays").with_query(replay_query)
+        replay_url = (API_URL / "replays").with_query(replay_query)
         replay_task = tg.create_task(session.get(replay_url))
 
     track = await track_task.result().json()
@@ -104,7 +106,7 @@ async def play_track(request: Request):
     session = app["client_session"]
 
     async with session.get(
-        f"https://tmnf.exchange/trackplay/{trackid}", allow_redirects=False
+        BASE_URL.joinpath("trackplay", trackid), allow_redirects=False
     ) as res:
         location = res.headers["location"]
         return render_manialink("redirect.xml", request, {"target": location})
