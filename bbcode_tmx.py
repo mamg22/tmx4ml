4cd580797cd0d7f8b9fe627b649fbea602d707c4
mamg22 <45301823+mamg22@users.noreply.github.com>
2025-08-17 15:39:43 -0400

    bbcode: support new tags

--- bbcode_tmx.py
+++ bbcode_tmx.py
@@ -11,24 +11,34 @@ parser = bbcode.Parser(
     url_template="$l[{href}]{text}$l",
 )
 
-parser.add_simple_formatter("b", "$s%(value)s$s")
-parser.add_simple_formatter("i", "$i%(value)s$i")
-parser.add_simple_formatter("u", "$i%(value)s$i")
-parser.add_simple_formatter("s", "$n%(value)s$m")
-parser.add_simple_formatter("img", "$l[%(value)s]<image>$l")
+parser.add_simple_formatter("b", "$s%(value)s$s", strip=True)
+parser.add_simple_formatter("i", "$i%(value)s$i", strip=True)
+parser.add_simple_formatter("u", "$i%(value)s$i", strip=True)
+parser.add_simple_formatter("s", "$n%(value)s$m", strip=True)
+parser.add_simple_formatter("img", "$l[%(value)s]<image>$l", strip=True)
+parser.add_simple_formatter("tmx", "$oTrackmania$d22Exchange$g$o", standalone=True)
+parser.add_simple_formatter("mx", "$oMania$7dfExchange$g$o", standalone=True)
+
+
+TAG_TO_ITEM = {
+    "track": "track",
+    "user": "user",
+    "pack": "trackpack"
+}
 
 
 def format_item(
     tag_name: str, value: str, options: dict[str, str], parent, context: dict[str, str]
 ) -> str:
     value = value.strip()
-    url = yarl.URL(context["origin"]) / context["site"] / tag_name / value
+    url = yarl.URL(context["origin"]) / context["site"] / TAG_TO_ITEM[tag_name.lower()] / value
 
     return f"$h[{url}]<{tag_name}:{value}>$h"
 
 
 parser.add_formatter("track", format_item)
 parser.add_formatter("user", format_item)
+parser.add_formatter("pack", format_item)
 
 
 def format_bbcode(source: str, origin: str, site: str) -> str:
